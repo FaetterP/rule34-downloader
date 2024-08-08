f8c8eaa91d624974e9cdcab5f118f656a08c1a20
FaetterP <56697273+FaetterP@users.noreply.github.com>
2024-08-08 17:35:21 +0300

    Добавлено создание папки

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { getPagesQueue } from "./queues/getPages";
-import "./workers"
+import fs from "node:fs";
+import "./workers";
 
 async function launch(categories: string[]) {
+  const downloadsFolder = `${__dirname}/../downloads`;
+  if (!fs.existsSync(downloadsFolder)) {
+    fs.mkdirSync(downloadsFolder);
+  }
+
   for (const category of categories) {
     getPagesQueue.add(`get pages '${categories}'`, { category });
   }

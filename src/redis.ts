a5ead0ba113ce6d0023b48c1c340239d5a78519a
FaetterP <56697273+FaetterP@users.noreply.github.com>
2025-07-11 23:43:37 +0300

    Добавлены энвы для подключения к редису

--- src/redis.ts
+++ src/redis.ts
@@ -1,3 +1,16 @@
 import IORedis from "ioredis";
 
-export const redisConnection = new IORedis({ maxRetriesPerRequest: null });
+const {
+  REDIS_HOST = "127.0.0.1",
+  REDIS_PORT = "6379",
+  REDIS_PASSWORD,
+  REDIS_DB = "0",
+} = process.env;
+
+export const redisConnection = new IORedis({
+  host: REDIS_HOST,
+  port: Number(REDIS_PORT),
+  password: REDIS_PASSWORD,
+  db: Number(REDIS_DB),
+  maxRetriesPerRequest: null,
+});
